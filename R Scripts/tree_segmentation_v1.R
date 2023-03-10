5af7501467c2cec5f40d96d61a41b5bf86970dd0
Shea Holden <sheakholden@gmail.com>
2023-03-09 22:35:07 -0800

    Added LAS tiling and RGB point cloud rendering

--- R Scripts/tree_segmentation_v1.R
+++ R Scripts/tree_segmentation_v1.R
@@ -2,7 +2,7 @@
 library(lidR)
 library(sf)
 library(ggplot2)
-library(raster)
+library(terra) # instead of raster
 library(viridis)
 library(crayon)
 library(BiocManager)
@@ -17,10 +17,48 @@ library(rgl)
 
 message(green("\n[INFO] ", Sys.time(), " Reading .LAS files"))
 
+# =========================TESTING TILING===================
+
+lasC <- readLAScatalog("/Users/Shea/Desktop/COMP 4910/RGB Data/points.las")
+ortho <- terra::rast("/Users/Shea/Desktop/COMP 4910/RGB Data/rgb_map.tif")
+t_dir <- "/Users/Shea/Desktop/COMP 4910/RGB Data/Tiles"
+
+opt_output_files(lasC) <- paste0(t_dir, "/{XLEFT}_{YBOTTOM}_{ID}") # label outputs based on coordinates
+opt_chunk_size(lasC) <- 250 # square tile size in meters
+opt_chunk_buffer(lasC) <- 0
+tiles <- catalog_retile(lasC)
+tiles <- readLAScatalog(t_dir)
+
+plot(tiles, chunk = TRUE)
+# plotRGB(ortho)
+
+# Read only the specified files of the LAScatalog
+t_files <- list.files(t_dir, pattern = ".las$", full.names = TRUE)
+subset <- readLAS(t_files[1:10])
+
+plot(subset, bg = "white")
+
+norm_color <- function(l, o) # create user-defined function
+{
+  cn <- classify_noise(l, ivf(4, 5))
+  cn <- filter_poi(cn, Classification != LASNOISE) # denoise
+  gnd <- classify_ground(cn, csf(sloop_smooth = TRUE, class_threshold = 1, time_step = 0.65))
+  nl <- normalize_height(gnd, knnidw()) # normalize
+  # colorized <- merge_spatial(nl, o) # colorize
+  return(nl) # output
+}
+
+# las <- clip_circle(tiles, 680500, 5605500, 100)
+
+nlasrgb <- norm_color(subset, ortho) # apply user defined function
+
+plot(nlasrgb, color = "RGB", bg = "white", size = 2)
+
+# ====================================================
+
 # TODO: Update path to folder location on your local machine
-lasC <- readLAScatalog("/Users/Shea/Desktop/COMP 4910/RGB Data/LiDAR split files/-split1")
-las <- readLAS(lasC, select = "xyzr", filter = "-drop_z_below 0")
-# las <- readLAS("/Users/Shea/Desktop/COMP 4910/RGB Data/points.las", select = "xyzr", filter = "-drop_z_below 0")
+# lasC <- readLAScatalog("/Users/Shea/Desktop/COMP 4910/RGB Data/LiDAR split files/-split1")
+# las <- readLAS(lasC, filter = "-drop_z_below 0")
 
 message(green("\n[INFO] ", Sys.time(), " Checking .LAS file"))
 las_check(las)
@@ -64,6 +102,7 @@ plot(nlas, bg = "white")
 # hist(filter_ground(nlas)$Z, breaks = seq(-50, 50, 0.01), main = "", xlab = "Elevation")
 
 col <- height.colors(25)
+# TODO: Find maximum res (lower number) by 6/ point cloud density
 chm <- rasterize_canopy(nlas, res = 0.5, pitfree(thresholds = c(0, 60), max_edge = c(0, 1.5)))
 # plot(chm, col = col)
 
@@ -84,6 +123,7 @@ las <- segment_trees(las, algo) # segment point cloud
 
 crowns <- crown_metrics(las, func = .stdtreemetrics, geom = "concave")
 plot(crowns["convhull_area"], main = "Crown area (concave hull)")
+
 
 # =======attempting to transfer tree segments to rgb orthomosaic=======
 trees_sp <- as_Spatial(crowns)
