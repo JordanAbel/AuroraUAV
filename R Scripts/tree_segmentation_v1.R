ce2c0f5eeaa1eb1a9d989d8451c963bfe49d270d
Shea Holden <sheakholden@gmail.com>
2023-03-17 21:57:35 -0700

    Added percentage for amount of points dropped when de-noising. Added code to write section of rgb ortho with segments overlayed to png file with treeID numbers (WIP). Testing code for retaining full resolution of RGB image. Testing code for manipulating data frame to add/ edit species column. Testing code for merging segments and rgb image. May not be necessary.

--- R Scripts/tree_segmentation_v1.R
+++ R Scripts/tree_segmentation_v1.R
@@ -3,19 +3,9 @@ library(lidR)
 library(sf)
 library(ggplot2)
 library(terra) # instead of raster
-library(viridis)
 library(crayon)
 library(sp)
-library(rayshader)
 library(viridis)
-
-# library(BiocManager)
-# library(lidR.li2012enhancement)
-
-# # Install custom package
-# install.packages("BiocManager")
-# BiocManager::install("EBImage")
-# devtools::install_github("mcoghill/lidR.li2012enhancement")
 
 # Increase virtual memory limit of R environment
 Sys.setenv(R_MAX_VSIZE = "100Gb")
@@ -24,9 +14,9 @@ Sys.getenv("R_MAX_VSIZE") # verify that change has taken effect
 message(green("\n[INFO] ", Sys.time(), " Reading .LAS files"))
 
 # TODO: Update paths for local machine
-lasC <- readLAScatalog("C:/Users/Jorda/OneDrive/Documents/University/Comp 3710/AuroraUAV/RGB Data/points.las") # Full ortho .las point cloud
-ortho <- terra::rast("C:/Users/Jorda/OneDrive/Documents/University/Comp 3710/AuroraUAV/RGB Data/rgb_map.tif") # Full RGB ortho map
-t_dir <- "C:/Users/Jorda/OneDrive/Documents/University/Comp 3710/AuroraUAV/RGB Data/Tiles" # directory to store tiles after tiling step
+lasC <- readLAScatalog("/Users/Shea/Desktop/COMP 4910/RGB Data/points.las") # Full ortho .las point cloud
+ortho <- terra::rast("/Users/Shea/Desktop/COMP 4910/RGB Data/rgb_map.tif") # Full RGB ortho map
+t_dir <- "/Users/Shea/Desktop/COMP 4910/RGB Data/Tiles" # directory to store tiles after tiling step
 
 opt_output_files(lasC) <- paste0(t_dir, "/{XLEFT}_{YBOTTOM}_{ID}") # label outputs based on coordinates
 opt_chunk_size(lasC) <- 250 # square tile size in meters
@@ -39,14 +29,18 @@ tiles <- readLAScatalog(t_dir) # Read tile folder
 
 # Read only the specified subset of tiles - for processing speeds & testing purposes
 t_files <- list.files(t_dir, pattern = ".las$", full.names = TRUE)
-subset <- readLAS(t_files[14:16]) # t_files[n:m] = file position in order. Delete “[n:m]” to read entire catalog.
-# subset <- readLAS(t_files)
+# subset <- readLAS(t_files[8:10]) # t_files[n:m] = file position in order. Delete “[n:m]” to read entire catalog.
+subset <- readLAS(t_files)
 
 # Check subset tiles. Will indicate if there are any invalidities.
 message(green("\n[INFO] ", Sys.time(), " Checking point cloud"))
 las_check(subset)
 
 # plot(subset, bg = "white")
+
+# ===========================================================================================
+# POINT CLOUD PRE-PROCESSING
+# ===========================================================================================
 
 # Function to plot crossection of point cloud. Illustrates ground/ non-ground points
 plot_crossection <- function(las,
@@ -65,14 +59,15 @@ plot_crossection <- function(las,
 }
 
 # Pre-processing function to denoise and normalize point cloud. Boolean option params to generate plots
-noise_norm <- function(l, p = TRUE, subplots = FALSE)
+noise_norm <- function(l, p, subplots)
 {
   message(green("\n[INFO] ", Sys.time(), " noise_norm() - Processing", npoints(l), "points"))
   message(green("\n[INFO] ", Sys.time(), " noise_norm() - Classifying & filtering noise points"))
   cn <- classify_noise(l, sor(19, 0.9))
   cn <- filter_poi(cn, Classification != LASNOISE) # drop noise points
   diff <- npoints(l) - npoints(cn)
-  message(green("\n[INFO] ", Sys.time(), " noise_norm() - Removed", diff, "noise points"))
+  per <- round((diff / npoints(l)) * 100, 2)
+  message(green("\n[INFO] ", Sys.time(), " noise_norm() - Removed", diff, "noise points (", per, "% )"))
 
   message(green("\n[INFO] ", Sys.time(), " noise_norm() - Classifying ground points"))
   gnd <- classify_ground(cn, csf(sloop_smooth = TRUE, class_threshold = 1, time_step = 0.65))
@@ -103,19 +98,15 @@ noise_norm <- function(l, p = TRUE, subplots = FALSE)
 
 nnsub <- noise_norm(subset, FALSE, FALSE)
 
-# ========================================
+# ===========================================================================================
+# POINT CLOUD POST-PROCESSING
+# ===========================================================================================
 
+las <- nnsub
 c <- height.colors(25)
-# TODO: Tune base_res. May change depending on subset
+# Tune base_res. May change depending on subset
 base_res <- (6 / density(nnsub))
-print(base_res)
-chm <- rasterize_canopy(nnsub, res = base_res,
-                        pitfree(subcircle = 0.15,
-                                thresholds = c(0,5,10,15,20,25,30,35,40),
-                                max_edge = c(0, 2)
-                        )
-)
-plot(chm, col = c)
+base_res <- (6 / density(nnsub)) + 0.1
 
 # This function will create the window size based off of the tree height
 variable_ws <- function(x) {
@@ -134,29 +125,76 @@ chm_pitfree_05_2 <- rasterize_canopy(las, 0.5, pitfree(
 
 ttops_chm_pitfree_05_2 <- locate_trees(chm_pitfree_05_2, lmf(variable_ws))
 
-plot(chm_pitfree_05_2, main = "CHM PITFREE 2", col = col); plot(sf::st_geometry(ttops_chm_pitfree_05_2), add = T, pch =3)
+plot(chm_pitfree_05_2, main = "CHM PITFREE 2", col = c)
+plot(sf::st_geometry(ttops_chm_pitfree_05_2), add = T, pch =3)
 
 algo <- dalponte2016(chm_pitfree_05_2, ttops_chm_pitfree_05_2)
 las <- segment_trees(las, algo) # segment point cloud
-plot(las, bg = "white", color = "treeID") # visualize trees
+# plot(las, bg = "white", color = "treeID") # visualize trees
+
+# Testing displaying ID's
+# trees_clean <- subset(las, !is.na(las@data$treeID))
+# plot(trees_clean, bg = "white", color = "treeID") # visualize trees
+# text(trees_clean@data$X, trees_clean@data$Y, labels = trees_clean@data$treeID, cex = 0.8, pos = 1)
+
 
 crowns <- crown_metrics(las, func = .stdtreemetrics, geom = "concave")
 # TODO: Modify to drop segments of either too little points or small area
 q_val <- quantile(crowns$convhull_area, 0.12)
 crowns <- crowns[crowns$convhull_area >= q_val,]
-plot(crowns["convhull_area"], main = "Crown area (concave hull)", col = viridis(10))
+# plot(crowns["convhull_area"], main = "Crown area (concave hull)", col = viridis(10))
 
-# =======attempting to transfer tree segments to rgb orthomosaic=======
-trees_sp <- as_Spatial(crowns)
-trees_sp <- spTransform(trees_sp, crs(ortho))
-# trees_sf <- st_as_sf(trees_sp)
-ortho_rgb_cropped <- crop(ortho, extent(trees_sp), res = NULL)
-# trees_raster <- rasterize(trees_sf, ortho_rgb_cropped)
+# ======Testing adding column to crowns data table======
+ct <- crowns
 
-plotRGB(ortho_rgb_cropped, r = 1, g = 2, b = 3, stretch = "lin", add = FALSE)
-# plotRGB(ortho, r = 1, g = 2, b = 3, stretch = "lin", add = FALSE)
-plot(trees_sp, add = TRUE, border = "red", lwd = 1)
+# Adding new column
+ct$species <- 1 # add int value representing species to every row
 
+# deleteing column
+ct$species <- NULL
+
+# modifying rows
+ct$species <- 2 # update all rows of species column
+ct$species[ct$treeID == 2100] <- 1 # updating species value of treeID 4
+ct$species[ct$treeID %in% c(3, 5, 7)] <- 3 # updating all rows with treeID 3, 5, 7 to 3
+
+head(ct)
+
+# ===exporting data===
+
+st_write(ct, "labelled_polygons_test.geojson", driver = "GeoJSON")
+
+# below code to create trees_sp has to be run first
+# st_write(trees_sp, "spatial_polygons_test.geojson", driver = "GeoJSON") # doesnt work due to sp format.
+
+# =========================================
+
+# Overlay tree segments to corresponding rgb orthomosaic section
+trees_sp <- spTransform(as_Spatial(crowns), crs(ortho))
+ortho_cropped <- crop(ortho, extent(subset))
+
+png(filename="plot1.png", width = 6000, height = 6000, units = "px") # initialize png output for plot
+
+plotRGB(ortho_cropped, r = 1, g = 2, b = 3, stretch = "lin", add = FALSE)
+plot(trees_sp, add = TRUE, border = "red", lwd = 1.5)
+text(trees_sp, labels = trees_sp@data$treeID, cex = 1.1, col = "red")
+dev.off() # save png after plotting data
+
+
+# plot(trees_sp, border = "red", lwd = 1.5)
+
+
+# =====Testing merging polygons with rgb for ML algo data=====
+# labeled <- merge(ortho_rgb_cropped, trees_sp)
+trees_rast <- rasterize(trees_sp, ortho_cropped, field = 1)
+
+st_crs(trees_sp) <- st_crs(ortho_cropped)
+
+sv_trees <- as(trees_sp, "SpatVector")
+vals <- terra::extract(ortho_cropped, sv_trees)
+
+
+terra::writeRaster(labeled, "labelled_rgb_test_1.png")
 
 
 # ===================================================================
