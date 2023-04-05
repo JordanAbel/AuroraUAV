42d52314ac89ea1563ee01b35c4ea5d125beca00
Shea Holden <sheakholden@gmail.com>
2023-04-04 19:42:28 -0700

    More work on GUI. Added multithreading and live text stream from R script. Work out of v2 version of tree seg.

--- GUI.py
+++ GUI.py
@@ -1,7 +1,8 @@
 import tkinter as tk
 from tkinter import filedialog
+from tkinter.font import Font
 from PIL import ImageTk, Image
-import subprocess
+import subprocess, os, threading, sys
 
 
 class MainWindow(tk.Frame):
@@ -10,8 +11,15 @@ class MainWindow(tk.Frame):
 		self.master = master
 		self.grid()
 		self.create_gui()
+		self.r_thread = None
 	
 	def create_gui(self):
+		
+		# TODO: delete these paths after testing
+		o_path = "/Users/Shea/Desktop/COMP 4910/RGB Data/rgb_map.tif"
+		pc_path = "/Users/Shea/Desktop/COMP 4910/RGB Data/points.las"
+		
+		font = Font(family="Helvetica", size=26, weight="bold")
 		
 		# add image background
 		self.image = Image.open("forest_bg copy.png")
@@ -21,75 +29,117 @@ class MainWindow(tk.Frame):
 		self.master.geometry("{}x{}".format(self.image.size[0], self.image.size[1]))
 		
 		# create containers for individual rows
+		title_container = tk.Frame(root, bg="")
 		browse_container = tk.Frame(root, bg="")
 		tog_container = tk.Frame(root, bg="")
 		in_container = tk.Frame(root, bg="")
 		btn_container = tk.Frame(root, bg="")
+		text_container = tk.Frame(root)
+		
+		# create title
+		self.title = tk.Label(title_container, text="Tree Identification Software v2", font=font)
+		self.title.grid(row=0, column=0, sticky="nsew")
 		
 		# create file browsers
-		self.ortho_label = tk.Label(browse_container, text="Orthomosaic:", bg="systemTransparent")
-		self.ortho_path = tk.Label(browse_container, text="No Orthomosaic (.TIF) selected", bg="systemTransparent")
-		self.ortho_button = tk.Button(browse_container, text="Browse", command=self.browse_ortho, bg="systemTransparent")
+		self.ortho_label = tk.Label(browse_container, text="Orthomosaic:")
+		self.ortho_path = tk.Label(browse_container, text=o_path)
+		# self.ortho_path = tk.Label(browse_container, text="No Orthomosaic (.TIF) selected", bg="systemTransparent")
+		self.ortho_button = tk.Button(browse_container, text="Browse", command=self.browse_ortho)
 		self.ortho_label.grid(row=0, column=0, sticky="nsew")
 		self.ortho_path.grid(row=0, column=1, sticky="nsew")
 		self.ortho_button.grid(row=0, column=2, sticky="nsew")
 		
-		self.pc_label = tk.Label(browse_container, text="Point Cloud:", bg="systemTransparent")
-		self.pc_path = tk.Label(browse_container, text="No Point Cloud (.LAS) selected", bg="systemTransparent")
-		self.pc_button = tk.Button(browse_container, text="Browse", command=self.browse_pc, bg="systemTransparent")
+		self.pc_label = tk.Label(browse_container, text="Point Cloud:")
+		self.pc_path = tk.Label(browse_container, text=pc_path)
+		# self.pc_path = tk.Label(browse_container, text="No Point Cloud (.LAS) selected", bg="systemTransparent")
+		self.pc_button = tk.Button(browse_container, text="Browse", command=self.browse_pc)
 		self.pc_label.grid(row=1, column=0, sticky="nsew")
 		self.pc_path.grid(row=1, column=1, sticky="nsew")
 		self.pc_button.grid(row=1, column=2, sticky="nsew")
 		
 		# create plot toggles
-		self.toggle_label = tk.Label(tog_container, text="Output Plots:", bg="systemTransparent")
-		self.toggle_label.grid(row=1, column=0, columnspan=5, sticky="nsew")
+		self.toggle_label = tk.Label(tog_container, text="Output Plots:")
+		self.toggle_label.grid(row=1, column=0, columnspan=8, sticky="nsew")
 		
+		# 2D Cross section
+		self.cross_var = tk.BooleanVar(value=False)
+		self.cross_button = tk.Checkbutton(tog_container, text="Cross", variable=self.cross_var)
+		self.cross_button.grid(row=2, column=0)
+		
+		# 3d Digital terrain model
 		self.dtm_var = tk.BooleanVar(value=False)
-		self.dtm_button = tk.Checkbutton(tog_container, text="DTM", variable=self.dtm_var, bg="systemTransparent")
-		self.dtm_button.grid(row=2, column=0)
+		self.dtm_button = tk.Checkbutton(tog_container, text="DTM", variable=self.dtm_var)
+		self.dtm_button.grid(row=2, column=1)
 		
+		# 3D Normalized (flat ground) point cloud. Heat Map colouring
+		self.n_normalized_var = tk.BooleanVar(value=False)
+		self.n_normalized_button = tk.Checkbutton(tog_container, text="Non-norm", variable=self.n_normalized_var)
+		self.n_normalized_button.grid(row=2, column=2)
+		
+		# 3D Normalized (flat ground) point cloud. Heat Map colouring
 		self.normalized_var = tk.BooleanVar(value=False)
-		self.normalized_button = tk.Checkbutton(tog_container, text="Normalized", variable=self.normalized_var, bg="systemTransparent")
-		self.normalized_button.grid(row=2, column=1)
+		self.normalized_button = tk.Checkbutton(tog_container, text="Norm", variable=self.normalized_var)
+		self.normalized_button.grid(row=2, column=3)
 		
-		self.canopy_var = tk.BooleanVar(value=False)
-		self.canopy_button = tk.Checkbutton(tog_container, text="Canopy", variable=self.canopy_var, bg="systemTransparent")
-		self.canopy_button.grid(row=2, column=2)
-		
-		self.segments_var = tk.BooleanVar(value=False)
-		self.segments_button = tk.Checkbutton(tog_container, text="Segments", variable=self.segments_var, bg="systemTransparent")
-		self.segments_button.grid(row=2, column=3)
-		
+		# 3D rgb point cloud
 		self.rgb_var = tk.BooleanVar(value=False)
-		self.rgb_button = tk.Checkbutton(tog_container, text="RGB", variable=self.rgb_var, bg="systemTransparent")
+		self.rgb_button = tk.Checkbutton(tog_container, text="RGB", variable=self.rgb_var)
 		self.rgb_button.grid(row=2, column=4)
 		
+		# 2D located trees
+		self.canopy_var = tk.BooleanVar(value=False)
+		self.canopy_button = tk.Checkbutton(tog_container, text="Canopy", variable=self.canopy_var)
+		self.canopy_button.grid(row=2, column=5)
+		
+		# 3D segments
+		self.segments_var = tk.BooleanVar(value=False)
+		self.segments_button = tk.Checkbutton(tog_container, text="Segments", variable=self.segments_var)
+		self.segments_button.grid(row=2, column=6)
+		
+		# 2D segments - rgb overlay
+		self.overlay_var = tk.BooleanVar(value=False)
+		self.overlay_button = tk.Checkbutton(tog_container, text="Overlay", variable=self.overlay_var)
+		self.overlay_button.grid(row=2, column=7)
+		
 		# create inputs
+		self.toggle_label = tk.Label(in_container, text="Point Cloud Tile Params:")
+		self.toggle_label.grid(row=0, column=0, columnspan=2, sticky="nsew")
+		
 		self.tile_input_var = tk.StringVar(value="250")
-		self.tile_size_label = tk.Label(in_container, text="Tile Size (m):", bg="systemTransparent")
+		self.tile_size_label = tk.Label(in_container, text="Tile Size (m):")
 		self.tile_size_label.grid(row=3, column=0)
-		self.tile_size_entry = tk.Entry(in_container, textvariable=self.tile_input_var, bg="systemTransparent")
+		self.tile_size_entry = tk.Entry(in_container, textvariable=self.tile_input_var)
 		self.tile_size_entry.grid(row=3, column=1)
 		
 		self.buff_input_var = tk.StringVar(value="0")
-		self.tile_buffer_label = tk.Label(in_container, text="Tile Buffer (m):", bg="systemTransparent")
+		self.tile_buffer_label = tk.Label(in_container, text="Tile Buffer (m):")
 		self.tile_buffer_label.grid(row=4, column=0)
-		self.tile_buffer_entry = tk.Entry(in_container, textvariable=self.buff_input_var, bg="systemTransparent")
+		self.tile_buffer_entry = tk.Entry(in_container, textvariable=self.buff_input_var)
 		self.tile_buffer_entry.grid(row=4, column=1)
 		
 		# create options for tables
 		# TODO: options for outputting 2 column table for manual labelling, etc
 		
-		# create run button
-		run_button = tk.Button(btn_container, text="Run", command=self.run_segmentation, bg="systemTransparent")
-		run_button.grid(row=5, column=1)
+		# create run/ stop button
+		self.run_button = tk.Button(btn_container, text="Run", command=self.run_segmentation)
+		self.run_button.grid(row=0, column=1)
+		self.stop_button = tk.Button(btn_container, text="Stop", command=self.stop_r_thread)
+		self.stop_button.grid(row=0, column=2)
 		
-		# place container on
+		# create output text box
+		self.toggle_button = tk.Button(text_container, text="Show Output", command=self.toggle_textbox)
+		self.toggle_button.grid(row=0, column=0)
+		self.textbox = tk.Text(text_container, height=15)
+		self.textbox.grid(row=1, column=0)
+		
+		# place containers on GUI
+		title_container.place(relx=0.5, rely=0.2, anchor="center")
 		browse_container.place(relx=0.5, rely=0.3, anchor="center")
 		tog_container.place(relx=0.5, rely=0.4, anchor="center")
-		in_container.place(relx=0.5, rely=0.5, anchor="center")
-		btn_container.place(relx=0.5, rely=0.6, anchor="center")
+		in_container.place(relx=0.5, rely=0.53, anchor="center")
+		btn_container.place(relx=0.5, rely=0.63, anchor="center")
+		text_container.place(relx=0.5, rely=0.9, anchor="center")
+		self.textbox.grid_remove()
 		
 	def browse_ortho(self):
 		self.ortho_p = filedialog.askopenfilename()
@@ -98,14 +148,41 @@ class MainWindow(tk.Frame):
 	def browse_pc(self):
 		self.pc_p = filedialog.askopenfilename()
 		self.pc_path.config(text=self.pc_p)
+		
+	def toggle_textbox(self):
+		# toggle the visibility of the text box
+		if self.textbox.winfo_ismapped():
+			self.textbox.grid_remove()
+			self.toggle_button.config(text="Show Output")
+		else:
+			self.textbox.grid()
+			self.toggle_button.config(text="Hide")
+	
+	def stop_r_thread(self):
+		if self.r_thread is not None:
+			self.r_thread.stop_flag.set()
+			self.run_button.config(state="normal")
 	
 	def run_segmentation(self):
-		# Get the values from the GUI
+		self.run_button.config(state="disabled")
+		self.textbox.delete("1.0", tk.END)
+		self.textbox.grid()
+		# Get the input values from the GUI
+		# Paths
 		ortho_path = self.ortho_path.cget("text")
 		pc_path = self.pc_path.cget("text")
+		# Inputs
 		tile_size = int(self.tile_size_entry.get())
 		tile_buffer = int(self.tile_buffer_entry.get())
-		normalize = self.normalized_var.get()
+		# Toggles
+		cross = self.cross_var.get()
+		dtm = self.dtm_var.get()
+		n_normalized = self.n_normalized_var.get()
+		normalized = self.normalized_var.get()
+		rgb = self.rgb_var.get()
+		canopy = self.canopy_var.get()
+		segments = self.segments_var.get()
+		overlay = self.overlay_var.get()
 		
 		# Input checks before running script
 		# Checks to do:
@@ -129,20 +206,98 @@ class MainWindow(tk.Frame):
 		
 		else:
 			# Set the command line arguments for the R script
-			# args = ["Rscript", "tree_segmentation_v1.R", ortho_path, str(tile_size), str(normalize)]
-			print("would run R script")
+			r_script = "./tree_segmentation_v2.R"
 			
-			# Run the R script using subprocess
+			# Make the R script executable (equivalent to running "chmod +x r_script" in terminal)
+			os.chmod(r_script, 0o755)
+			
+			# args = ["Rscript", r_script, str(ortho_path), str(pc_path), str(tile_size), str(tile_buffer),
+			#         str(dtm), str(normalized), str(canopy), str(segments), str(rgb)]
+			
+			cmd = ["Rscript", r_script]
+			args = [str(ortho_path), str(pc_path), str(tile_size), str(tile_buffer), str(cross), str(dtm),
+			        str(n_normalized), str(normalized), str(rgb), str(canopy), str(segments), str(overlay)]
+			
+			# Create RThread and start it
+			self.r_thread = RThread(cmd, self.textbox, args=args)
+			self.r_thread.start()
+
+
+class RThread(threading.Thread):
+	def __init__(self, cmd, text_box, args=None):
+		threading.Thread.__init__(self)
+		self.cmd = cmd
+		self.text_box = text_box
+		self.args = args
+		self.stop_flag = threading.Event()
+	
+	def run(self):
+		cmd = self.cmd + self.args if self.args else self.cmd
+		try:
+			process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+			
+			# # Run the R script using subprocess
 			# try:
-			# 	output = subprocess.check_output(args, stderr=subprocess.STDOUT)
-			# 	print(output.decode())
+			# 	# subprocess.run(args)
+			# 	# output = subprocess.check_output(args, stderr=subprocess.STDOUT)
+			# 	# print(output.decode())
+			# 	process = subprocess.Popen(["Rscript", r_script, *args], stdout=subprocess.PIPE,
+			# 	                           stderr=subprocess.STDOUT, universal_newlines=True)
+			#
+			# 	# Read the output line by line and print it to the console
+			# 	for line in process.stdout:
+			# 		print(line, end='')
+			#
+			# 		self.textbox.insert(tk.END, line)
+			#
+			# 	# TODO: get text stream working properly
+			# 	# # read the output from the pipe
+			# 	# output = proc.stdout.read()
+			# 	#
+			# 	# # insert the output into the text box
+			# 	# self.textbox.insert(tk.END, output)
+			#
+			# 	# output, _ = process.communicate()
+			# 	# self.textbox.insert("1.0", output) # "1.0" to add to top, tk.END to add to bottom
+			# 	# self.textbox.see(tk.END)
+			# 	# print(output, end='')
+			# 	#
+			# 	# # continuously read the output from the pipe and insert it into the text box
+			# 	# for line in iter(process.stdout.readline, ""):
+			# 	# 	self.textbox.insert(tk.END, line)
+			# 	# 	self.textbox.see(tk.END)
+			# 	# 	self.parent.update_idletasks()
+			# 	#
+			# 	#
+			# 	# process.stdout.close()
+			# 	# process.wait()
+			#
+			#
 			# except subprocess.CalledProcessError as e:
 			# 	print("Error:", e.output.decode())
+			
+			while True:
+				output = process.stdout.readline()
+				if output == b'' and process.poll() is not None:
+					break
+				if output:
+					print(output)
+					self.text_box.insert("1.0", output)
+				if self.stop_flag.is_set():
+					process.terminate()
+					text = "Process terminated by user"
+					self.text_box.insert("1.0", text)
+					break
+			rc = process.poll()
+			return rc
+		
+		except subprocess.CalledProcessError as e:
+			print("Error:", e.output.decode())
 
 
 if __name__ == "__main__":
 	root = tk.Tk()
-	root.title("Aurora UAV: Tree ID v1")
+	root.title("Aurora UAV: Tree ID v2")
 	
 	app = MainWindow(master=root)
 	# app.pack(fill=tk.BOTH, expand=True)
---
+++ tree_segmentation_v2.R
@@ -0,0 +1,322 @@
+
+
+# Loading args from command GUI
+args <- commandArgs(trailingOnly = TRUE)
+
+# Paths
+ortho_path <- args[1]
+las_path <- args[2]
+
+# Tile params
+t_size <- args[3]
+t_buffer <- args[4]
+
+# plots booleans
+cross_plt <- args[5]
+dtm_plt <- args[6]
+non_norm <- args[7]
+norm_plt <- args[8]
+rgb_plt <- args[9]
+canopy_plt <- args[10]
+seg_plt <- args[11]
+overlay__plt <- args[12]
+
+plt_list <- args[5:12]
+
+print(plt_list)
+print(ortho_path)
+print(las_path)
+
+# Automatically install and load required packages into R session
+pkg_file <- "R_requirements.txt"
+pkg_list <- readLines(pkg_file)
+
+# Check for the existence of each package and install if necessary
+cran_mirror <- "https://cran.r-project.org" # Set the CRAN mirror to use for package installations
+for (pkg_name in pkg_list) {
+  if (!require(pkg_name, character.only = TRUE)) {
+    install.packages(pkg_name, repos = cran_mirror)
+    if (!require(pkg_name, character.only = TRUE)) {
+      stop("Failed to install package: ", pkg_name)
+    }
+  }
+  library(pkg_name, character.only = TRUE)
+}
+
+# Automatically increase virtual memory limit of R environment
+Sys.setenv(R_MAX_VSIZE = "100Gb")
+Sys.getenv("R_MAX_VSIZE") # verify that change has taken effect
+
+message(green("\n[INFO] ", Sys.time(), " Reading point cloud"))
+lasC <- readLAScatalog(las_path) # Full ortho point cloud
+ortho <- terra::rast(ortho_path) # Full RGB ortho map
+
+# Function to read point cloud file and perform tiling.
+pc_read_tile <- function(cat, t_size, t_buff) {
+  # Create directory to store tiles if it does not exist yet
+  if(!file.exists("Tiles")){
+    dir.create("Tiles")
+    t_dir <- "Tiles"
+  } else {
+    t_dir <- "Tiles"
+  }
+
+  # Check if directory is empty to determine if tiling has been done. Perform tiling if it is.
+  if(length(list.files(t_dir)) == 0) {
+    message(green("\n[INFO] ", Sys.time(), " Tiling point cloud. This may take a while."))
+    opt_output_files(cat) <- paste0(t_dir, "/{XLEFT}_{YBOTTOM}_{ID}") # label outputs based on coordinates
+    opt_chunk_size(cat) <- t_size # square tile size in meters
+    opt_chunk_buffer(cat) <- t_buff # buffer around each tile
+    catalog_retile(cat) # Tile full ortho point cloud.
+  }
+
+  # Read only the specified subset of tiles - for processing speeds & testing purposes
+  t_files <- list.files(t_dir, pattern = ".las$", full.names = TRUE)
+  subset <- readLAS(t_files[8:10]) # t_files[n:m] = file position in order. Delete “[n:m]” to read entire catalog.
+  # subset <- readLAS(t_files) # TODO: delete small subset line above to always do full file
+
+  return(subset)
+}
+
+subset <- pc_read_tile(lasC, t_size = t_size, t_buff = t_buffer)
+
+# Check subset tiles. Will indicate if there are any invalidities.
+message(green("\n[INFO] ", Sys.time(), " Checking point cloud"))
+las_check(subset)
+
+# plot(subset, bg = "white")
+
+# ===========================================================================================
+# POINT CLOUD PRE-PROCESSING
+# ===========================================================================================
+
+# Function to plot crossection of point cloud. Illustrates ground/ non-ground points
+plot_crossection <- function(las,
+                             p1 = c(min(las@data$X), mean(las@data$Y)),
+                             p2 = c(max(las@data$X), mean(las@data$Y)),
+                             width = 4, colour_by = NULL)
+{
+  colour_by <- enquo(colour_by)
+  data_clip <- clip_transect(las, p1, p2, width)
+  p <- ggplot(data_clip@data, aes(X,Z)) + geom_point(size = 0.5) + coord_equal() + theme_minimal()
+
+  if (!is.null(colour_by))
+    p <- p + aes(color = !!colour_by) + labs(color = "")
+
+  return(p)
+}
+
+# Pre-processing function to denoise and normalize point cloud
+noise_norm <- function(l)
+{
+  message(green("\n[INFO] ", Sys.time(), " noise_norm() - Processing", npoints(l), "points"))
+  message(green("\n[INFO] ", Sys.time(), " noise_norm() - Classifying & filtering noise points"))
+  cn <- classify_noise(l, sor(19, 0.9))
+  cn <- filter_poi(cn, Classification != LASNOISE) # drop noise points
+  diff <- npoints(l) - npoints(cn)
+  per <- round((diff / npoints(l)) * 100, 2)
+  message(green("\n[INFO] ", Sys.time(), " noise_norm() - Removed", diff, "noise points (", per, "% )"))
+
+  message(green("\n[INFO] ", Sys.time(), " noise_norm() - Classifying ground points"))
+  gnd <- classify_ground(cn, csf(sloop_smooth = TRUE, class_threshold = 1, time_step = 0.65))
+
+  message(green("\n[INFO] ", Sys.time(), " noise_norm() - Normalizing height"))
+  nl <- normalize_height(gnd, knnidw()) # normalize
+  nl <- filter_poi(nl, Z >= 0) # drop points below zero
+
+  out <- list(nl, gnd, cn)
+
+  print("DONE NOISE NORM")
+  return(out) # output
+}
+
+nn_list <- noise_norm(subset)
+
+nl <- nn_list[1]
+gnd <- nn_list[2]
+cn <- nn_list[3]
+
+# Generate specified subplots
+# Generate specified subplots
+cross_plt <- as.logical(plt_list[1])
+dtm_plt <- as.logical(plt_list[2])
+n_norm_plt <- as.logical(plt_list[3])
+norm_plt <- as.logical(plt_list[4])
+rgb_plt <- as.logical(plt_list[5])
+
+if (cross_plt == TRUE) {
+  message(green("\n[INFO] ", Sys.time(), "Plotting cross section"))
+  plot_crossection(nl, colour_by = factor(Classification))
+}
+if (dtm_plt == TRUE) {
+  message(green("\n[INFO] ", Sys.time(), "Plotting digital terrian model"))
+  plot_dtm3d((rasterize_terrain(gnd, res = 1, tin())), bg = "white", main = "Terrain Model")
+}
+if (n_norm_plt == TRUE) {
+  message(green("\n[INFO] ", Sys.time(), "Plotting non-normalized point cloud (Heat map colouring)"))
+  plot(cn, bg = "white", main = "Non-normalized Point Cloud")
+}
+if (norm_plt == TRUE) {
+  message(green("\n[INFO] ", Sys.time(), "Plotting normalized point cloud (Heat map colouring)"))
+  plot(nl, bg = "white", main = "Normalized Point Cloud")
+}
+if (rgb_plt == TRUE) {
+  message(green("\n[INFO] ", Sys.time(), "Plotting point cloud (RGB colouring)"))
+  plot(nl, color = "RGB", bg = "white", size = 3, main = "RGB Point Cloud")
+}
+
+
+#
+# # ===========================================================================================
+# # POINT CLOUD POST-PROCESSING
+# # ===========================================================================================
+
+# las <- nn_list[1]
+# c <- height.colors(25)
+# # Tune base_res. May change depending on subset
+# base_res <- (6 / density(nnsub))
+# base_res <- (6 / density(nnsub)) + 0.1
+#
+# # This function will create the window size based off of the tree height
+# variable_ws <- function(x) {
+#   y <- 2.6 * (-(exp(-0.08*(x-2)) - 1)) + 3
+#   y[x < 2] <- 3
+#   y[x > 20] <- 5
+#   return(y)
+# }
+#
+# # Pitfree with and subcircle tweak
+# chm_pitfree_05_2 <- rasterize_canopy(las, 0.5, pitfree(
+#   subcircle = 0.15,
+#   thresholds = c(0,5,10,15,20,25,30,35,40),
+#   max_edge = c(0, 2)
+# ), pkg = "terra")
+#
+# ttops_chm_pitfree_05_2 <- locate_trees(chm_pitfree_05_2, lmf(variable_ws))
+#
+# plot(chm_pitfree_05_2, main = "CHM PITFREE 2", col = c)
+# plot(sf::st_geometry(ttops_chm_pitfree_05_2), add = T, pch =3)
+#
+# algo <- dalponte2016(chm_pitfree_05_2, ttops_chm_pitfree_05_2)
+# las <- segment_trees(las, algo) # segment point cloud
+# # plot(las, bg = "white", color = "treeID") # visualize trees
+#
+# # Testing displaying ID's
+# # trees_clean <- subset(las, !is.na(las@data$treeID))
+# # plot(trees_clean, bg = "white", color = "treeID") # visualize trees
+# # text(trees_clean@data$X, trees_clean@data$Y, labels = trees_clean@data$treeID, cex = 0.8, pos = 1)
+#
+#
+# crowns <- crown_metrics(las, func = .stdtreemetrics, geom = "concave")
+# q_val <- quantile(crowns$convhull_area, 0.12)
+# crowns <- crowns[crowns$convhull_area >= q_val,]
+# # plot(crowns["convhull_area"], main = "Crown area (concave hull)", col = viridis(10))
+#
+# # ======Crowns data frame manipulation and export for manual labelling======
+# #TODO: Remove all ct lines. work with oroginal crowns DF
+# ct <- crowns
+#
+# ct$species <- 0 # Add empty column to make manual work easier
+#
+# ct_export <- ct[, c("treeID", "species")] # Select only treeID and species columns
+# ct_export$geometry <- NULL # Delete geometry column (this was somehow staying included in previous line)
+#
+# write.csv(ct_export, file = "segment_list.csv", row.names = FALSE) # Write to .csv for manual identification
+#
+# # Import csv and add column to data frame after manual labelling is completed.
+# labelled <- read.csv("segment_list.csv")
+#
+# ct$species <- labelled$species[match(ct$treeID, labelled$treeID)]
+#
+# # Below are examples of how to manipulate data frame data in R
+# # ct$species[ct$treeID == 2100] <- 1 # updating species value of treeID 4
+# # ct$species[ct$treeID %in% c(3, 5, 7)] <- 3 # updating all rows with treeID 3, 5, 7 to 3
+#
+# # ========================================================
+#
+# # Overlay tree segments to corresponding rgb orthomosaic section
+# trees_sp <- spTransform(as_Spatial(crowns), crs(ortho))
+# ortho_cropped <- crop(ortho, extent(subset))
+#
+# png(filename= "cropped_overlay.png", width = 29812, height = 33605, res = 300, units = "px") # initialize png output for plot
+#
+# plotRGB(ortho_cropped, r = 1, g = 2, b = 3, stretch = "lin", add = FALSE)
+# plot(trees_sp, border = "red", lwd = 1.5, bg = "transparent")
+# text(trees_sp, labels = trees_sp@data$treeID, cex = 1.1, col = "red")
+# dev.off() # save png after plotting data
+# # *** Above lines work for overlaying, but output rgb image at low resolution ***
+#
+#
+#
+# library(stars)
+#
+# trees_sp <- spTransform(as_Spatial(crowns), crs(ortho))
+# cropped <- crop(ortho, trees_sp)
+#
+# m <- as.data.frame(cropped, xy = TRUE)
+# merged <- terra::merge(trees_sp, m)
+#
+#
+# png(filename="res_test_2.png", width = 6000, height = 6000, units = "px") # initialize png output for plot
+# # plot(o_stars, rgb = 1:3)
+# plotRGB(r)
+# dev.off()
+#
+# cropped_stars <- st_as_stars(cropped)
+#
+# plot(cropped_stars)
+#
+# trees_stars <- st_as_stars(trees_sp, crs = st_crs(cropped_stars))
+#
+#
+#
+# png(filename="cropped_ortho1.png", width = 29812, height = 33605, res = 300, units = "px") # initialize png output for plot
+#
+# plot(cropped_stars)
+#
+# dev.off()
+#
+# plot(cropped_stars, rgb = 1:3, alpha(0.5))
+# plot(trees_sp, border = "red", lwd = 1.5, add = TRUE)
+# # text(trees_sp, labels = trees_sp@data$treeID, cex = 1.1, col = "red")
+#
+#
+# dev.off()
+#
+# plotRGB(cr, r = 1, g = 2, b = 3)
+#
+# plot(cropped_stars, rgb = 1:3, add = FALSE)
+# # plotRGB(cropped, r = 1, g = 2, b = 3, stretch = "lin", add = FALSE)
+# # plot(trees_sp, add = TRUE, border = "red", lwd = 1.5)
+# # text(trees_sp, labels = trees_sp@data$treeID, cex = 1.1, col = "red")
+#
+# # plotRGB(ortho_r)
+#
+# dev.off()
+#
+#
+#
+#
+# # =====Testing merging polygons with rgb for ML algo data=====
+# # labeled <- merge(ortho_rgb_cropped, trees_sp)
+# trees_rast <- rasterize(trees_sp, ortho_cropped, field = 1)
+#
+# st_crs(trees_sp) <- st_crs(ortho_cropped)
+#
+# sv_trees <- as(trees_sp, "SpatVector")
+# vals <- terra::extract(ortho_cropped, sv_trees)
+#
+#
+# terra::writeRaster(labeled, "labelled_rgb_test_1.png")
+#
+#
+# # ===================================================================
+# metrics <- crown_metrics(las, ~list(z_max = max(Z), z_mean = mean(Z))) # calculate tree metrics
+# head(metrics)
+#
+# # plot(metrics["z_max"], pal = hcl.colors, pch = 19) # plot using z_max
+#
+# plot(metrics["z_max"], pal = hcl.colors)
+#
+# tree110 <- filter_poi(las, treeID == 282)
+# plot(tree110, bg = "white")
