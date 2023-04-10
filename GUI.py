54a5aae08740cb311b8b7b5e0a7f80fa69dff929
Shea Holden <sheakholden@gmail.com>
2023-04-10 15:06:21 -0700

    Updates to GUI to get file paths from system, not code

--- GUI.py
+++ GUI.py
@@ -1,3 +1,5 @@
+import queue
+import time
 import tkinter as tk
 from tkinter import filedialog, ttk
 from tkinter.font import Font
@@ -6,6 +8,8 @@ import subprocess, os, threading, sys
 
 
 class MainWindow(tk.Frame):
+	root_path = getattr(sys, '_MEIPASS', os.getcwd())
+	
 	def __init__(self, master=None):
 		super().__init__(master)
 		self.master = master
@@ -18,11 +22,13 @@ class MainWindow(tk.Frame):
 		# TODO: delete these paths after testing
 		o_path = "/Users/Shea/Desktop/COMP 4910/RGB Data/rgb_map.tif"
 		pc_path = "/Users/Shea/Desktop/COMP 4910/RGB Data/points.las"
+		save_dir_path = "/Users/Shea/Desktop"
 		
-		font = Font(family="Helvetica", size=26, weight="bold")
+		title_font = Font(family="Helvetica", size=26, weight="bold")
 		
 		# add image background
-		self.image = Image.open("forest_bg copy.png")
+		self.image_path = os.path.join(self.root_path, 'forest_bg1.png')
+		self.image = Image.open(self.image_path)
 		self.background_image = ImageTk.PhotoImage(self.image)
 		self.background_label = tk.Label(self.master, image=self.background_image)
 		self.background_label.place(x=0, y=0, relwidth=1, relheight=1)
@@ -30,19 +36,22 @@ class MainWindow(tk.Frame):
 		
 		# create containers for individual rows
 		title_container = tk.Frame(root, bg="")
+		ver_container = tk.Frame(root, bg="")
 		browse_container = tk.Frame(root, bg="")
 		tog_container = tk.Frame(root, bg="")
 		in_container = tk.Frame(root, bg="")
 		btn_container = tk.Frame(root, bg="")
-		text_container = tk.Frame(root)
+		text_container = tk.Frame(root, bg="")
 		
-		# create title
-		self.title = tk.Label(title_container, text="Tree Identification Software v2", font=font)
+		# create title and version number
+		self.title = tk.Label(title_container, text="Forest Metrics Software", font=title_font)
 		self.title.grid(row=0, column=0, sticky="nsew")
+		self.ver = tk.Label(ver_container, text="v 2.1.0")
+		self.ver.grid(row=0, column=0)
 		
 		# create file browsers
 		self.ortho_label = tk.Label(browse_container, text="Orthomosaic:")
-		self.ortho_path = tk.Label(browse_container, text=o_path)
+		self.ortho_path = tk.Label(browse_container, text=o_path)  # TODO: delete and uncomment below line
 		# self.ortho_path = tk.Label(browse_container, text="No Orthomosaic (.TIF) selected", bg="systemTransparent")
 		self.ortho_button = tk.Button(browse_container, text="Browse", command=self.browse_ortho)
 		self.ortho_label.grid(row=0, column=0, sticky="nsew")
@@ -50,12 +59,20 @@ class MainWindow(tk.Frame):
 		self.ortho_button.grid(row=0, column=2, sticky="nsew")
 		
 		self.pc_label = tk.Label(browse_container, text="Point Cloud:")
-		self.pc_path = tk.Label(browse_container, text=pc_path)
+		self.pc_path = tk.Label(browse_container, text=pc_path)  # TODO: Delete and uncomment below line
 		# self.pc_path = tk.Label(browse_container, text="No Point Cloud (.LAS) selected", bg="systemTransparent")
 		self.pc_button = tk.Button(browse_container, text="Browse", command=self.browse_pc)
 		self.pc_label.grid(row=1, column=0, sticky="nsew")
 		self.pc_path.grid(row=1, column=1, sticky="nsew")
 		self.pc_button.grid(row=1, column=2, sticky="nsew")
+		
+		self.save_dir_label = tk.Label(browse_container, text="Output Directory:")
+		self.save_dir_path = tk.Label(browse_container, text=save_dir_path)  # TODO: delete and uncomment below line
+		# self.save_dir_path = tk.Label(browse_container, text="No output directory selected", bg="systemTransparent")
+		self.save_dir_button = tk.Button(browse_container, text="Browse", command=self.browse_sd)
+		self.save_dir_label.grid(row=2, column=0, sticky="nsew")
+		self.save_dir_path.grid(row=2, column=1, sticky="nsew")
+		self.save_dir_button.grid(row=2, column=2, sticky="nsew")
 		
 		# create plot toggles
 		self.toggle_label = tk.Label(tog_container, text="Output Plots:")
@@ -142,14 +159,15 @@ class MainWindow(tk.Frame):
 		self.textbox.grid(row=1, column=0)
 		
 		# place containers on GUI
-		title_container.place(relx=0.5, rely=0.2, anchor="center")
-		browse_container.place(relx=0.5, rely=0.3, anchor="center")
+		title_container.place(relx=0.5, rely=0.15, anchor="center")
+		ver_container.place(relx=0.95, rely=0.05, anchor="center")
+		browse_container.place(relx=0.5, rely=0.27, anchor="center")
 		tog_container.place(relx=0.5, rely=0.4, anchor="center")
 		in_container.place(relx=0.5, rely=0.53, anchor="center")
 		btn_container.place(relx=0.5, rely=0.63, anchor="center")
 		text_container.place(relx=0.5, rely=0.855, anchor="center")
 		self.textbox.grid_remove()
-		
+	
 	def browse_ortho(self):
 		self.ortho_p = filedialog.askopenfilename()
 		self.ortho_path.config(text=self.ortho_p)
@@ -157,7 +175,11 @@ class MainWindow(tk.Frame):
 	def browse_pc(self):
 		self.pc_p = filedialog.askopenfilename()
 		self.pc_path.config(text=self.pc_p)
-		
+	
+	def browse_sd(self):
+		self.sd_p = filedialog.askopenfilename()
+		self.save_dir_path.config(text=self.sd_p)
+	
 	def toggle_textbox(self):
 		# toggle the visibility of the text box
 		if self.textbox.winfo_ismapped():
@@ -167,19 +189,29 @@ class MainWindow(tk.Frame):
 			self.textbox.grid()
 			self.toggle_button.config(text="Hide")
 	
+	def update_textbox(self, text):
+		self.textbox.insert(tk.END, text)
+	
 	def stop_r_thread(self):
 		if self.r_thread is not None:
 			self.r_thread.stop_flag.set()
 			self.run_button.config(state="normal")
 	
+	def r_stopped(self, text):
+		self.run_button.config(state="normal")
+		self.textbox.insert(tk.END, text)
+	
 	def run_segmentation(self):
 		self.run_button.config(state="disabled")
 		self.textbox.delete("1.0", tk.END)
-		self.toggle_textbox()
+		self.textbox.grid()
+		self.textbox.insert(tk.END, "Commencing Processing")
+		self.toggle_button.config(text="Hide")
 		# Get the input values from the GUI
 		# Paths
 		ortho_path = self.ortho_path.cget("text")
 		pc_path = self.pc_path.cget("text")
+		sd_path = self.save_dir_path.cget("text")
 		# Inputs
 		tile_size = int(self.tile_size_entry.get())
 		tile_buffer = int(self.tile_buffer_entry.get())
@@ -195,6 +227,7 @@ class MainWindow(tk.Frame):
 		
 		# Input checks before running script
 		# Checks to do:
+		# - Save dir path
 		# - extreme tile/ buffer sizes
 		# - file type of input files
 		# - No plots selected
@@ -203,87 +236,52 @@ class MainWindow(tk.Frame):
 			# Display an error message
 			tk.messagebox.showerror("Error",
 			                        "Tile size must be greater than 0.\nBuffer size must be greater than or equal to 0")
+			self.run_button.config(state="normal")
 			return
 		if ortho_path == "No Orthomosaic (.TIF) selected":
 			# Display an error message
 			tk.messagebox.showerror("Error", "Orthomosaic file not selected")
+			self.run_button.config(state="normal")
 			return
 		if pc_path == "No Point Cloud (.LAS) selected":
 			# Display an error message
 			tk.messagebox.showerror("Error", "Point Cloud file not selected")
+			self.run_button.config(state="normal")
 			return
 		
 		else:
 			# Set the command line arguments for the R script
-			r_script = "./tree_segmentation_v2.R"
+			self.r_script = os.path.join(self.root_path, 'tree_segmentation_v2.R')
+			print(self.r_script)
+			# r_script = "tree_segmentation_v2.R"
 			
 			# Make the R script executable (equivalent to running "chmod +x r_script" in terminal)
-			os.chmod(r_script, 0o755)
+			os.chmod(self.r_script, 0o755)
 			
-			# args = ["Rscript", r_script, str(ortho_path), str(pc_path), str(tile_size), str(tile_buffer),
-			#         str(dtm), str(normalized), str(canopy), str(segments), str(rgb)]
-			
-			cmd = ["Rscript", r_script]
-			args = [str(ortho_path), str(pc_path), str(tile_size), str(tile_buffer), str(cross), str(dtm),
-			        str(n_normalized), str(normalized), str(rgb), str(canopy), str(segments), str(overlay)]
+			cmd = ["Rscript", self.r_script]
+			args = [str(ortho_path), str(pc_path), str(sd_path), str(self.root_path),
+			        str(tile_size), str(tile_buffer), str(cross), str(dtm), str(n_normalized),
+			        str(normalized), str(rgb), str(canopy), str(segments), str(overlay)]
 			
 			# Create RThread and start it
-			self.r_thread = RThread(cmd, self.textbox, args=args)
+			self.r_thread = RThread(cmd, self.textbox, args=args, output_callback=self.r_stopped)
 			self.r_thread.start()
 
 
 class RThread(threading.Thread):
-	def __init__(self, cmd, text_box, args=None):
+	
+	def __init__(self, cmd, text_box, args, output_callback):
 		threading.Thread.__init__(self)
 		self.cmd = cmd
 		self.text_box = text_box
 		self.args = args
 		self.stop_flag = threading.Event()
+		self.output_callback = output_callback
 	
 	def run(self):
 		cmd = self.cmd + self.args if self.args else self.cmd
 		try:
 			process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-			
-			# # Run the R script using subprocess
-			# try:
-			# 	# subprocess.run(args)
-			# 	# output = subprocess.check_output(args, stderr=subprocess.STDOUT)
-			# 	# print(output.decode())
-			# 	process = subprocess.Popen(["Rscript", r_script, *args], stdout=subprocess.PIPE,
-			# 	                           stderr=subprocess.STDOUT, universal_newlines=True)
-			#
-			# 	# Read the output line by line and print it to the console
-			# 	for line in process.stdout:
-			# 		print(line, end='')
-			#
-			# 		self.textbox.insert(tk.END, line)
-			#
-			# 	# TODO: get text stream working properly
-			# 	# # read the output from the pipe
-			# 	# output = proc.stdout.read()
-			# 	#
-			# 	# # insert the output into the text box
-			# 	# self.textbox.insert(tk.END, output)
-			#
-			# 	# output, _ = process.communicate()
-			# 	# self.textbox.insert("1.0", output) # "1.0" to add to top, tk.END to add to bottom
-			# 	# self.textbox.see(tk.END)
-			# 	# print(output, end='')
-			# 	#
-			# 	# # continuously read the output from the pipe and insert it into the text box
-			# 	# for line in iter(process.stdout.readline, ""):
-			# 	# 	self.textbox.insert(tk.END, line)
-			# 	# 	self.textbox.see(tk.END)
-			# 	# 	self.parent.update_idletasks()
-			# 	#
-			# 	#
-			# 	# process.stdout.close()
-			# 	# process.wait()
-			#
-			#
-			# except subprocess.CalledProcessError as e:
-			# 	print("Error:", e.output.decode())
 			
 			while True:
 				output = process.stdout.readline() + process.stderr.readline()
@@ -296,11 +294,13 @@ class RThread(threading.Thread):
 					self.text_box.see(tk.END)
 				if self.stop_flag.is_set():
 					process.terminate()
-					text = "Process terminated by user"
-					self.text_box.insert(tk.END, text)
 					break
-			rc = process.poll()
-			return rc
+			process.wait()
+			
+			# Notify the GUI that the R script has finished
+			self.output_callback("R process stopped")
+		# rc = process.poll()
+		# return rc
 		
 		except subprocess.CalledProcessError as e:
 			print("Error:", e.output.decode())
@@ -313,7 +313,7 @@ class Tooltip:
 		self.widget.bind("<Enter>", self.show)
 		self.widget.bind("<Leave>", self.hide)
 		self.widget.bind("<ButtonPress>", self.hide)
-		
+	
 	def show(self, event=None):
 		x, y, cx, cy = self.widget.bbox("insert")
 		x += self.widget.winfo_rootx() + 25
@@ -323,18 +323,17 @@ class Tooltip:
 		self.tw.wm_geometry("+%d+%d" % (x, y))
 		label = tk.Label(self.tw, text=self.text, justify="left", relief="solid", borderwidth=1)
 		label.pack(ipadx=1)
-		
+	
 	def hide(self, event=None):
 		if self.tw:
 			self.tw.destroy()
-		    
-		    
+
+
 if __name__ == "__main__":
 	root = tk.Tk()
-	root.title("Aurora UAV: Tree ID v2")
+	root.title("Aurora UAV: Forest Metrics")
 	
 	app = MainWindow(master=root)
-	# app.pack(fill=tk.BOTH, expand=True)
 	app.mainloop()
 
 
